1733f531e647c9d96ab2d64f4a6c86678e1c540a
andrewfellman <andrewfellman@gmail.com>
2023-10-25 16:11:01 -0400

    added program outline

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def main():
+    pick = None
+
+    while pick != '3':
+
+        print(f'Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
+        pick = input('Please enter an option: ')
+
+        if pick == '1':
+            pass #FIXME implement incode function
+
+        if pick == '2':
+            pass #FIXME implement decode function
+
+if __name__ == "__main__":
+    main()
