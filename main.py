65f3aa79bb58b1f4fc0ba1dbdffa3b447a466d06
andrewfellman <andrewfellman@gmail.com>
2023-10-25 16:31:15 -0400

    added code to incode string

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 
 def main():
     pick = None
-
+    store_password = ""
     while pick != '3':
 
-        print(f'Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
+        print(f'\nMenu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         pick = input('Please enter an option: ')
 
         if pick == '1':
-            pass #FIXME implement incode function
+            password = input('Please enter your password to encode: ')
+            for num in password:
+                num = int(num)
+                num += 3
+                num = str(num)
+                store_password += num
+            print('Your password has been encoded and stored!')
+
 
         if pick == '2':
             pass #FIXME implement decode function
